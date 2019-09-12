165980f45e1f431d07308b8f06a59db371eff2da
Kwame <bkwamesakyi@gmail.com>
2019-09-12 10:15:41 -0400

    Made some shapes. Edited code to add forward line. Added a pentagon, square,circle, and half of a star.

---
+++ pentagon.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(150)
+
+
+turtle.right(135)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.exitonclick()
--- shapes.py
+++ shapes.py
@@ -1,5 +1,51 @@
 import turtle
+turtle.speed(0)
 turtle.forward(100)
 turtle.left(135)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(70)
+
+turtle.penup()
+turtle.right(50)
 turtle.forward(100)
+
+turtle.pendown()
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(142)
+
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+
+turtle.circle(50)
+
+turtle.penup()
+turtle.right(90)
+turtle.forward(100)
+
+turtle.pendown()
+for x in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.penup()
+turtle.forward(200)
+
+turtle.pendown()
+turtle.left(70)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.right(150)
+turtle.forward(100)
+
+
 turtle.exitonclick()
